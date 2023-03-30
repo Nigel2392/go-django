cce81d2e08e9499f58e6634cf4d166e10cd7c3b9
Nigel van Keulen <Nigelvk@itextra.nl>
2023-03-30 14:10:18 +0200

    better go mod tidy on startproject

--- core/app/tool/tool.go
+++ core/app/tool/tool.go
@@ -1,9 +1,15 @@
 package tool
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 	"os/exec"
+	"strconv"
+	"strings"
 
 	"github.com/Nigel2392/go-django/core/flag"
 	"github.com/Nigel2392/go-django/core/httputils"
@@ -44,29 +50,183 @@ func StartProject(v flag.Value) error {
 	if err := createFile(".env", []byte(env_str_generated_secret)); err != nil {
 		return err
 	}
-	return initGoMod(v)
+	return initGoMod(v.String())
 }
 
-func initGoMod(v flag.Value) error {
-	var projectName = v.String()
+type Tag struct {
+	Name    string `json:"name"`
+	Integer int    `json:"-"`
+}
+
+type Tags []Tag
+
+func (t Tags) Len() int {
+	return len(t)
+}
+
+func DecodeTags(data io.Reader) Tags {
+	var tags = Tags{}
+	decoder := json.NewDecoder(data)
+	err := decoder.Decode(&tags)
+	if err != nil {
+		panic(err)
+	}
+	return tags
+}
+func (t Tags) initInts() {
+	for i := 0; i < len(t); i++ {
+		var tag = strings.TrimPrefix(t[i].Name, "v")
+		var tag_parts = strings.Split(tag, ".")
+		var newTag = strings.Join(tag_parts, "")
+		for len(newTag) < 4 {
+			newTag += "0"
+		}
+		var newTagInt, err = strconv.Atoi(newTag)
+		if err != nil {
+			panic(errors.New("Could not create tags: " + err.Error()))
+		}
+		t[i].Integer = newTagInt
+	}
+}
+
+func (t Tags) Descending() {
+	t.initInts()
+	for i := 0; i < len(t); i++ {
+		for j := i + 1; j < len(t); j++ {
+			if t[i].Integer < t[j].Integer {
+				t[i], t[j] = t[j], t[i]
+			}
+		}
+	}
+}
+
+func (t Tags) Ascending() {
+	t.initInts()
+	for i := 0; i < len(t); i++ {
+		for j := i + 1; j < len(t); j++ {
+			if t[i].Integer > t[j].Integer {
+				t[i], t[j] = t[j], t[i]
+			}
+		}
+	}
+}
+
+func (t Tags) Latest() Tag {
+	t.initInts()
+	t.Descending()
+	return t[0]
+}
+
+var GITHUB_TAG_URL = "https://api.github.com/repos/Nigel2392/go-django/tags"
+var GITHUB_REPO_URL = "https://github.com/Nigel2392/go-django"
+
+// Initialize go.mod to get the latest version of the project.
+// This only works for github repositories with tags in the following format:
+func initGoMod(projectName string) error {
 	var name = httputils.NameFromPath(projectName)
 	var cmd = exec.Command("go", "mod", "init", "go-django/"+name)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-
 	if err := cmd.Run(); err != nil {
 		return err
 	}
 
-	cmd = exec.Command("go", "mod", "tidy")
+	var req, _ = http.NewRequest("GET", GITHUB_TAG_URL, nil)
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	// Decode the tags from the github api
+	var tagList = DecodeTags(resp.Body)
+	// Get the latest version of jsext
+	var latestTag = tagList.Latest()
+
+	var extra = ""
+	if len(latestTag.Name) > 1 {
+		var prefix = latestTag.Name[0]
+		if prefix == 'v' || prefix == 'V' {
+			extra = "/" + latestTag.Name[0:1]
+		}
+	}
+
+	if err := runCmd("go", "get", GITHUB_REPO_URL+extra+"@"+latestTag.Name); err != nil {
+		return err
+	}
+	if err := runCmd("go", "mod", "tidy"); err != nil {
+		return err
+	}
+	return nil
+}
+
+// Run a system command.
+func runCmd(name string, args ...string) error {
+	var cmd = exec.Command(name, args...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-
-	return cmd.Run()
+	var err = cmd.Run()
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func StartApp(v flag.Value) error {
 	var appName = v.String()
-	fmt.Println("Creating app: ", appName)
+	if err := createDir(appName); err != nil {
+		panic(err)
+	}
+	if err := os.Chdir(appName); err != nil {
+		panic(err)
+	}
+	createFile("urls.go", []byte(getURLSTemplate(httputils.NameFromPath(appName))))
+	createFile("views.go", []byte(getViewsTemplate(httputils.NameFromPath(appName))))
+	createFile("models.go", []byte(getModelsTemplate(httputils.NameFromPath(appName))))
 	return nil
 }
+
+func getURLSTemplate(appName string) string {
+	return fmt.Sprintf(urlsTemplate, appName, appName, appName, appName, appName, appName)
+}
+
+func getViewsTemplate(appName string) string {
+	return fmt.Sprintf(viewsTemplate, appName, appName)
+}
+
+func getModelsTemplate(appName string) string {
+	return fmt.Sprintf(modelsTemplate, appName, appName)
+}
+
+var urlsTemplate = `package %s
+
+import (
+	"github.com/Nigel2392/router/v3"
+)
+
+var %s_route = router.Group("/%s", "%s")
+
+func URLs() router.Registrar {
+	%s_route.Get("", index, "index")
+	return %s_route
+}`
+
+var viewsTemplate = `package %s
+
+import (
+	"github.com/Nigel2392/router/v3/request"
+)
+
+func index(r *request.Request) {
+	r.WriteString("Hello from %s")
+}`
+
+var modelsTemplate = `package %s
+
+import (
+	"github.com/Nigel2392/go-django/core/models"
+)
+
+// Declare your models here.
+type %s_model struct {
+	models.Model
+}`
