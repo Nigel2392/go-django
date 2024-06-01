0366635c5dfe27162041c224aa43447481a1fe45
Nigel van Keulen <nigel2392@gmail.com>
2024-06-01 11:49:32 +0200

    refactor templ

--- django/contrib/blocks/render.go
+++ django/contrib/blocks/render.go
@@ -1,31 +1,14 @@
 package blocks
 
 import (
-	"bytes"
-	"html/template"
+	"context"
 	"io"
 
 	"github.com/Nigel2392/django/core/ctx"
-	"github.com/Nigel2392/django/core/errs"
-	"github.com/Nigel2392/django/core/tpl"
 )
 
-func RenderBlockForm(w io.Writer, widget *BlockWidget, context *BlockContext, errors []error) error {
-	var (
-		byteBuf      = new(bytes.Buffer)
-		templateName = "blocks/widgets/block_widget.tmpl"
-		err          = widget.BlockDef.RenderForm(
-			byteBuf, context.ID, context.Name,
-			context.Value, errors, context,
-		)
-	)
-	if err != nil {
-		return errs.Wrap(err, "Error rendering block form")
-	}
-
-	context.BlockHTML = template.HTML(byteBuf.String())
-
-	return tpl.FRender(w, context, templateName)
+func RenderBlockForm(w io.Writer, widget *BlockWidget, ctx *BlockContext, errors []error) error {
+	return RenderBlockWidget(w, widget, ctx, errors).Render(context.Background(), w)
 }
 
 func RenderBlock(w io.Writer, block Block, value any, context ctx.Context) error {
