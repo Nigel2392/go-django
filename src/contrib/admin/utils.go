d11c55be295729a73a1379e5b2614da1987e55c1
Nigel van Keulen <nigel2392@gmail.com>
2024-11-05 12:29:11 +0100

    allow for easily retrieving admin ModelDefinitions with admin.FindDefinition

--- src/contrib/admin/utils.go
+++ src/contrib/admin/utils.go
@@ -3,6 +3,7 @@ package admin
 import (
 	"errors"
 	"net/http"
+	"reflect"
 
 	"github.com/Nigel2392/go-django/src/core/attrs"
 	"github.com/Nigel2392/go-django/src/core/except"
@@ -12,6 +13,30 @@ var (
 	ErrModelNotFound = errors.New("model not found")
 	ErrAppNotFound   = errors.New("app not found")
 )
+
+func FindDefinition(model attrs.Definer) *ModelDefinition {
+	var modelType = reflect.TypeOf(model)
+	if modelType.Kind() == reflect.Ptr {
+		modelType = modelType.Elem()
+	}
+
+	for head := AdminSite.Apps.Front(); head != nil; head = head.Next() {
+		var app = head.Value
+		for front := app.Models.Front(); front != nil; front = front.Next() {
+			var modelDef = front.Value
+			var typ = modelDef.rModel()
+			if typ.Kind() == reflect.Ptr {
+				typ = typ.Elem()
+			}
+
+			if typ == modelType {
+				return modelDef
+			}
+		}
+	}
+
+	return nil
+}
 
 func GetModelInstance(appName, modelName string, id interface{}) (attrs.Definer, error) {
 	app, ok := AdminSite.Apps.Get(appName)
