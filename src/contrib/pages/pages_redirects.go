074a04816bc619ebd987333c15c4a97949bdf7fc
Nigel van Keulen <nigel2392@gmail.com>
2025-02-10 10:34:51 +0100

    Add generic redirect route based on Page ID, redirecting to the page's live URL

---
+++ src/contrib/pages/pages_redirects.go
@@ -0,0 +1,37 @@
+package pages
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/Nigel2392/go-django/src/core/except"
+	"github.com/Nigel2392/go-django/src/permissions"
+	"github.com/Nigel2392/mux"
+)
+
+func redirectHandler(w http.ResponseWriter, r *http.Request) {
+	var variables = mux.Vars(r)
+	var pageID = variables.Get(PageIDVariableName)
+
+	if !permissions.HasPermission(r, "pages:redirect") || !pageApp.useRedirectHandler {
+		except.Fail(http.StatusForbidden, nil)
+		return
+	}
+
+	var id, err = strconv.Atoi(pageID)
+	if err != nil {
+		except.Fail(http.StatusBadRequest, err)
+		return
+	}
+
+	page, err := pageApp.QuerySet().GetNodeByID(
+		r.Context(), int64(id),
+	)
+	if err != nil {
+		except.Fail(http.StatusNotFound, err)
+		return
+	}
+
+	var pageUrl = URLPath(&page)
+	http.Redirect(w, r, pageUrl, http.StatusFound)
+}
