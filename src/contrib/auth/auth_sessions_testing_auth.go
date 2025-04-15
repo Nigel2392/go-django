af1f2229ace10d3a006d040f428dfc3bfcb2ebfc
Nigel van Keulen <nigel2392@gmail.com>
2025-04-15 14:26:30 +0200

    fixes to auth_sessions_testing_auth.go

--- src/contrib/auth/auth_sessions_testing_auth.go
+++ src/contrib/auth/auth_sessions_testing_auth.go
@@ -18,10 +18,11 @@ import (
 	"net/http"
 
 	models "github.com/Nigel2392/go-django/src/contrib/auth/auth-models"
+	django_signals "github.com/Nigel2392/go-django/src/signals"
 )
 
 func Login(r *http.Request, u *models.User) (*models.User, error) {
-	SIGNAL_USER_LOGGED_IN.Send(UserWithRequest{
+	django_signals.SIGNAL_USER_LOGGED_IN.Send(django_signals.UserWithRequest{
 		User: u,
 		Req:  r,
 	})
@@ -30,7 +31,7 @@ func Login(r *http.Request, u *models.User) (*models.User, error) {
 }
 
 func Logout(r *http.Request) error {
-	return SIGNAL_USER_LOGGED_OUT.Send(UserWithRequest{
+	return django_signals.SIGNAL_USER_LOGGED_OUT.Send(django_signals.UserWithRequest{
 		User: nil,
 		Req:  r,
 	})
