22723a6b435b4a36b6f5d8645aaa39a689fd5db9
Nigel van Keulen <nigel2392@gmail.com>
2025-07-11 09:59:45 +0200

    add unique update test

--- queries/test/unique_test.go
+++ queries/test/unique_test.go
@@ -24,4 +24,44 @@ func TestUniqueInsert(t *testing.T) {
 	} else {
 		t.Logf("Expected (and received) error when creating duplicate unique source: %v", err)
 	}
+
+	_, err = queries.GetQuerySet(&UniqueSource{}).Delete()
+	if err != nil {
+		t.Fatalf("Failed to delete unique source: %v", err)
+	}
+}
+
+func TestUniqueUpdate(t *testing.T) {
+	uniqueSources, err := queries.GetQuerySet(&UniqueSource{}).
+		BulkCreate([]*UniqueSource{
+			{Name: "Unique Source 1"},
+			{Name: "Unique Source 2"},
+		})
+
+	if err != nil {
+		t.Fatalf("Failed to create unique sources: %v", err)
+	}
+
+	uniqueSource1 := uniqueSources[0]
+	uniqueSource2 := uniqueSources[1]
+
+	updated, err := queries.GetQuerySet(&UniqueSource{}).
+		Update(&UniqueSource{
+			ID:   uniqueSource2.ID,
+			Name: uniqueSource1.Name,
+		})
+	if err == nil {
+		t.Fatalf("Expected error when updating unique source to duplicate name, got none")
+	} else {
+		t.Logf("Expected (and received) error when updating unique source to duplicate name: %v", err)
+	}
+
+	if updated != 0 {
+		t.Fatalf("Expected no rows to be updated, got %d", updated)
+	}
+
+	_, err = queries.GetQuerySet(&UniqueSource{}).Delete()
+	if err != nil {
+		t.Fatalf("Failed to delete unique source: %v", err)
+	}
 }
