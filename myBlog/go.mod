4570ce3c76025d0de438f829e21d728179c3140c
Nigel van Keulen <nigel2392@gmail.com>
2025-07-18 09:15:13 +0200

    setup rc28

---
+++ myBlog/go.mod
@@ -0,0 +1,5 @@
+module github.com/Nigel2392/my-blog
+
+go 1.24.2
+
+require github.com/Nigel2392/go-django v1.7.1-0.20250624173105-ab30533d8fd3 // indirect
---
+++ myBlog/src/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	django "github.com/Nigel2392/go-django/src"
+	"github.com/Nigel2392/go-django/src/contrib/admin"
+	"github.com/Nigel2392/go-django/src/contrib/auth"
+	"github.com/Nigel2392/go-django/src/contrib/pages"
+	"github.com/Nigel2392/go-django/src/contrib/reports"
+	"github.com/Nigel2392/go-django/src/contrib/messages"
+	"github.com/Nigel2392/go-django/queries/src/drivers"
+	auditlogs "github.com/Nigel2392/go-django/src/contrib/reports/audit_logs"
+	"github.com/Nigel2392/go-django/src/contrib/session"
+	"github.com/Nigel2392/go-django/src/core/logger"
+	myblog "github.com/Nigel2392/my-blog/src/myBlog"
+
+	_ "github.com/Nigel2392/go-django/src/contrib/pages/backend-sqlite"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	var db, err = drivers.Open("sqlite3", "./db.sqlite3")
+	if err != nil {
+		panic(err)
+	}
+
+	var app = django.App(
+		django.Configure(map[string]interface{}{
+			django.APPVAR_ALLOWED_HOSTS: []string{"*"},
+			django.APPVAR_DEBUG:         false,
+			django.APPVAR_HOST:          "0.0.0.0",
+			django.APPVAR_PORT:          "8080",
+			django.APPVAR_DATABASE: 	 db,
+			// django.APPVAR_RECOVERER: false,
+
+			"AUTH_EMAIL_LOGIN": true,
+		}),
+
+		django.Apps(
+			session.NewAppConfig,
+			messages.NewAppConfig,
+			auth.NewAppConfig,
+			admin.NewAppConfig,
+			pages.NewAppConfig,
+			auditlogs.NewAppConfig,
+			reports.NewAppConfig,
+			myblog.NewAppConfig,
+		),
+	)
+
+	pages.SetRoutePrefix("/pages")
+
+	err = app.Initialize()
+	if err != nil {
+		panic(err)
+	}
+
+	app.Log.SetLevel(
+		logger.DBG,
+	)
+
+	if err := app.Serve(); err != nil {
+		panic(err)
+	}
+}
