8e41c16b7e280e340fd6b41e114ef59b32364cb3
Nigel van Keulen <nigel2392@gmail.com>
2025-07-21 23:15:00 +0200

    Add script file to easily create new example projects

--- new-example.ps1
+++ new-example.ps1
@@ -1,4 +1,10 @@
 param(
     [Parameter(Mandatory = $true)]
-    [string]$Name,
+    [string]$Name
 )
+
+$WHERE = "./examples"
+$GOMODULE = "github.com/Nigel2392/go-django/examples/$Name"
+
+Invoke-Expression "go-django startproject -d $WHERE/$Name -m $GOMODULE $Name"
+Invoke-Expression "go work use $WHERE/$Name"
