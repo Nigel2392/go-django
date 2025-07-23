ec0abd323cc419bed9cdd16a5bdb9c616419998d
Nigel van Keulen <nigel2392@gmail.com>
2025-07-23 21:45:13 +0200

    remove sqlite database from formsapp

--- examples/formsapp/src/main.go
+++ examples/formsapp/src/main.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"context"
-
 	formsapp "github.com/Nigel2392/go-django/examples/formsapp/src/formsapp"
-	"github.com/Nigel2392/go-django/queries/src/drivers"
 	django "github.com/Nigel2392/go-django/src"
 	"github.com/Nigel2392/go-django/src/contrib/pages"
 	"github.com/Nigel2392/go-django/src/core/logger"
@@ -13,18 +10,12 @@ import (
 )
 
 func main() {
-	var db, err = drivers.Open(context.Background(), "sqlite3", "./db.sqlite3")
-	if err != nil {
-		panic(err)
-	}
-
 	var app = django.App(
 		django.Configure(map[string]interface{}{
 			django.APPVAR_ALLOWED_HOSTS: []string{"*"},
 			django.APPVAR_DEBUG:         false,
 			django.APPVAR_HOST:          "127.0.0.1",
 			django.APPVAR_PORT:          "8080",
-			django.APPVAR_DATABASE:      db,
 			// django.APPVAR_RECOVERER: false,
 
 		}),
